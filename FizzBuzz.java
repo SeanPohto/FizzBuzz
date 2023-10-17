b0d725566f3affc2022e26d292e31eb17ea86c4f
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-10-17 11:42:56 -0700

    Commit message

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,6 +2,16 @@ public class FizzBuzz
 {
     public static void main(String[] args)
     {
-        
+        for (int i=1; i <= 100; i++) {
+            if (i / 15 * 15 == i) {
+                System.out.println ("Fizzbuzz");
+            } else if (i / 5 * 5 == i) {
+                System.out.println ("Buzz");
+            } else if (i / 3 * 3 == i) {
+                System.out.println ("Fizz");
+            } else {
+                System.out.println (i);
+            }
+        }
     }
 }
